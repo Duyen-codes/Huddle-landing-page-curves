dcd94af8ca05a3fb3d70cf3ee3487961f9253a80
Nguyen Duyen <s2200128@BC-C02GR0BVQ6LR-MacBook-Air.local>
2022-02-27 08:22:09 +0200

    finished email validation

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 const emailInput = document.querySelector('input[type="email"');
-
 const subscribeButton = document.querySelector(".subscribe-btn");
 
 const newsletterForm = document.querySelector("form");
 const emailErrorMessage = document.querySelector(".email-error-msg");
-console.log(emailErrorMessage, subscribeButton, emailInput);
 
-const checkEmail = (event) => {
-  console.log(event.target);
+const validateEmail = (event) => {
   event.preventDefault();
-  if ((emailInput.value = "")) {
+  if (emailInput.value.length == 0) {
     emailInput.classList.add("error");
     emailErrorMessage.classList.add("show");
   }
+  return false;
 };
 
-subscribeButton.addEventListener("click", checkEmail);
+subscribeButton.addEventListener("click", validateEmail);
